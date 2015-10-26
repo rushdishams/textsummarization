0aaa6e477f7b40190e7a04253f5a1ea92b16588a
rushdishams <rushdecoder@gmail.com>
2015-10-26 17:10:04 +0000

    v-2.1.0: Change from previous version: Generates CSV file that contains article ID, original text and summary. Cleans text with a removeGarbage() method.

--- src/com/summary/ict/main/SummarizationBatch.java
+++ src/com/summary/ict/main/SummarizationBatch.java
@@ -1,8 +1,10 @@
 package com.summary.ict.main;
 
-
 import java.io.File;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.NoSuchElementException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -15,31 +17,33 @@ import com.boundary.sentence.TextContent;
 import net.sf.classifier4J.summariser.ISummariser;
 import net.sf.classifier4J.summariser.SimpleSummariser;
 
- 
 /**
- * The class to instantiate Simple Summariser object. Simple Summariser can be found in
- * Classify4j project
+ * The class to instantiate Simple Summariser object. Simple Summariser can be
+ * found in Classify4j project
+ * 
+ * Change from 2.0.0: Generates CSV file that contains article ID, original text and summary
+ * Cleans text with a removeGarbage() routine
+ * 
  * @author Rushdi Shams
- * @version 1.0 October 08, 2015.
+ * @version 2.1.0 October 26, 2015.
  *
  */
 public class SummarizationBatch {
-    //--------------------------------------------------------------------------------
-    //Instance variables
-    //--------------------------------------------------------------------------------
-    private static String summaryText;
-    private static ISummariser summarizer;
+	// --------------------------------------------------------------------------------
+	// Instance variables
+	// --------------------------------------------------------------------------------
+	private static String summaryText;
+	private static ISummariser summarizer;
 	private static Logger logger = Logger.getLogger("MyLog");
-    //--------------------------------------------------------------------------------
-    //Methods
-    //--------------------------------------------------------------------------------
-    
-    public static void initiateLogger(File file){
+	// --------------------------------------------------------------------------------
+	// Methods
+	// --------------------------------------------------------------------------------
+
+	public static void initiateLogger(File file) {
 		FileHandler fileHandler;
 		try {
 			// This block configure the logger with handler and formatter
-			fileHandler = new FileHandler(file.getParentFile().getAbsolutePath()+ "/"
-					+ "log.txt", true);
+			fileHandler = new FileHandler(file.getParentFile().getAbsolutePath() + "/" + "log.txt", true);
 			logger.addHandler(fileHandler);
 			SimpleFormatter formatter = new SimpleFormatter();
 			fileHandler.setFormatter(formatter);
@@ -50,67 +54,130 @@ public class SummarizationBatch {
 			e.printStackTrace();
 		}
 	}
-    /**
-     * Method to summarize a given string
-     * @param originalText is the text to be summarized
-     * @param summarySize is in integer denotes the sentences in the summary
-     * @return the summary of the sentence
-     */
-    public static String summarize(String originalText, int summarySize){
-        summarizer = new SimpleSummariser();
-        summaryText = summarizer.summarise(originalText, summarySize);
-        return summaryText;
-    }//end method
-    
-    /**
+
+	/**
+	 * Method to summarize a given string
+	 * 
+	 * @param originalText
+	 *            is the text to be summarized
+	 * @param summarySize
+	 *            is in integer denotes the sentences in the summary
+	 * @return the summary of the sentence
+	 */
+	public static String summarize(String originalText, int summarySize) {
+		summarizer = new SimpleSummariser();
+		try {
+			summaryText = summarizer.summarise(originalText, summarySize);
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		}
+		return summaryText;
+	}// end method
+
+	/**
 	 * Method to display banner on cmd line
 	 */
-	public static void showBanner(){
+	public static void showBanner() {
 		System.out.println("//----------------------------------------------------------------------------//");
-		System.out.println("\tText Summarizer v-1.0.0, 08/10/2015");
+		System.out.println("\tText Summarizer v-2.1.0, 26/10/2015");
 		System.out.println("\t\t\tAuthor: Rushdi Shams");
-		System.out.println("\tUSAGE: java -jar textsummary-1.0.0.jar directorypath/ [digit]");
-		System.out.println("\tUSAGE: [digit]: number of summary sentences to be generated.");
+		System.out.println("\tUSAGE: java -jar textsummary-2.1.0.jar directorypath/ [floating point number]");
+		System.out.println("\tUSAGE: [floating point number]: Percentage of Summary (e.g., 0.3 stands for 30%).");
 		System.out.println("//----------------------------------------------------------------------------//");
 	}
-    
-    public static void main(String[] args){
-    	showBanner();
-    	
-    	File folder = new File(args[0]);
-    	if(!folder.isDirectory()){
-    		System.out.println("Input must be a directory.");
-    		System.exit(1);
-    	}
+
+	public static void main(String[] args) {
+		showBanner();
+
+		Instant start = Instant.now();
+
+		File folder = new File(args[0]);
+		if (!folder.isDirectory()) {
+			System.out.println("Input must be a directory.");
+			System.exit(1);
+		}
 		File[] listOfFiles = folder.listFiles();
-		int summaryLength = Integer.parseInt(args[1]);
-		
-		for(int i = 0; i < listOfFiles.length; i ++){
-			
+		File summaryFile = new File(args[0] + "/" + "summary.csv");
+		String outputFileContent = "";
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			System.out.println("----Processing " + listOfFiles[i].getAbsolutePath() + "----\n");
+
 			String fileContent = "";
 			try {
-				fileContent = FileUtils.readFileToString(new File (listOfFiles[i].getAbsolutePath()));
+				fileContent = FileUtils.readFileToString(new File(listOfFiles[i].getAbsolutePath()));
 			} catch (IOException e) {
-				logger.info("Error in reading the text file\n");
+				logger.info("Error reading articl\n");
 			}
+			if (fileContent.length() == 0) {
+				System.out.println("File contains nothing\n");
+				continue;
+			}
+
+			fileContent = removeGarbage(fileContent);
+			TextContent t = new TextContent(); // creating TextContent object
+			t.setText(fileContent);
+			t.setSentenceBoundary();
+			String[] content = t.getSentence();
+			System.out.println("Number of Sentences: " + content.length);
+			int summaryLength = Math.round((float) content.length * Float.parseFloat(args[1]));
+			System.out.println("Summary Length: " + summaryLength);
 			String summary = summarize(fileContent, summaryLength).trim();
 			summary = summary.replaceAll("\r\n", " ");
-			TextContent t = new TextContent(); //creating TextContent object
-			t.setText(summary); //setting the text
-			t.setSentenceBoundary(); //detecting the sentence boundaries
-			String[] array = t.getSentence();//getting the sentences in an array
-			
+			t.setText(summary); // setting the text
+			t.setSentenceBoundary(); // detecting the sentence boundaries
+			String[] array = t.getSentence();// getting the sentences in an
+												// array
+
 			String summaryToBeWritten = "";
-			for (String str : array){
-				summaryToBeWritten += str +"\n";
+			for (String str : array) {
+				summaryToBeWritten += str + " ";
 			}
-			
-			File summaryFile = new File(FilenameUtils.getFullPath(listOfFiles[i].toString()) + listOfFiles[i].getName() +  "-automatic.txt");
+			// System.out.println(summaryToBeWritten);
+			String articleID = FilenameUtils.removeExtension(FilenameUtils.getName(listOfFiles[i].toString()));
+			// outputFileContent += "\"" + articleID + "\"" + "," + "\"" +
+			// fileContent.replaceAll("\r\n", " ").replaceAll("\r",
+			// "").replaceAll("\n", "").replaceAll("\"", "").replaceAll("\'",
+			// "").replaceAll(",", "") + "\"" + "," +
+			// summaryToBeWritten.replaceAll("\r\n", " ").replaceAll("\r",
+			// "").replaceAll("\n", "").replaceAll("\"", "").replaceAll("\'",
+			// "").replaceAll(",", "") + "\n";
+			outputFileContent += "\"" + articleID + "\"" + "," + "\"" + fileContent.replaceAll(",", "") + "\"" + ","
+					+ summaryToBeWritten.replaceAll(",", "") + "\n";
+
+			// File summaryFile = new
+			// File(FilenameUtils.getFullPath(listOfFiles[i].toString()) +
+			// listOfFiles[i].getName() + "-automatic.txt");
 			try {
-				FileUtils.write(summaryFile, summaryToBeWritten);
+				FileUtils.write(summaryFile, outputFileContent, true);
 			} catch (IOException e) {
 				logger.info("Error in writing summary");
 			}
+			outputFileContent = "";
 		}
-    }
-}//end class
+		// System.out.println(outputFileContent);
+		/*
+		 * try { FileUtils.write(summaryFile, outputFileContent); } catch
+		 * (IOException e) { logger.info("Error in writing summary"); }
+		 */
+		Instant end = Instant.now();
+		System.out.println("Completion time: " + Duration.between(start, end));
+	}
+
+	public static String removeGarbage(String sentence) {
+		sentence.replaceAll("[^\\p{ASCII}]", ""); // Strips off non-ascii
+													// characters
+		sentence = sentence.replaceAll("\\s+", " ");
+		sentence = sentence.replaceAll("\\p{Cntrl}", ""); // Strips off ascii
+															// control
+															// characters
+		sentence = sentence.replaceAll("[^\\p{Print}]", ""); // Strips off ascii
+																// non-printable
+																// characters
+		sentence = sentence.replaceAll("\\p{C}", ""); // Strips off
+														// non-printable
+														// characters from
+														// unicode
+		return sentence;
+	}// end method
+}// end class
